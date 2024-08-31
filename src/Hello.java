78f9688a77ecd3ea92c570e67bbf67f376085289
lnash <lnash@Lukes-Laptop.unl.edu>
2024-08-31 12:38:52 -0500

    Adding partner name and major

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Adam Khattak");
 		System.out.println("Computer Science\n");
-		
 		System.out.println("Luke Nash");
 		System.out.println("Computer Science");
 	}
