bb04d32103f9995a6c85726c772e02d836cc5d4c
lnash <lnash@Lukes-Laptop.unl.edu>
2024-09-03 11:01:02 -0500

    Added parter name and major

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Adam Khattak");
 		System.out.println("Computer Science\n");
+		
 		System.out.println("Luke Nash");
 		System.out.println("Computer Science");
 	}
